2d096c94e0fe9d027832f77695f46e8b869c4fc1
P€pperMCGamer$ <113534696+pmgdev64@users.noreply.github.com>
2025-05-30 12:33:13 +0700

    Add files via upload

--- src/GlobalSys/Renderer/GameEngine/Unity/UnityEngine.cs
+++ src/GlobalSys/Renderer/GameEngine/Unity/UnityEngine.cs
@@ -1,1 +1,14 @@
+using System;
+using System.Drawing;
+using System.Collections.Generic;
+using Cosmos.System;
+using Cosmos.System.Graphic;
+using Unity.Engine.CosmosGL;
 
+namespace Unity.Engine {
+    public class UnityEngine {
+        public UnityEngine() {
+            
+        }
+    }
+}

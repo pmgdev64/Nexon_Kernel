0cc10a46fdf878508fb053d578eb1c5007918cef
P€pperMCGamer$ <113534696+pmgdev64@users.noreply.github.com>
2025-05-30 12:38:46 +0700

    Add files via upload

--- src/GlobalSys/Application/Clock/Clock.cs
+++ src/GlobalSys/Application/Clock/Clock.cs
@@ -1,1 +1,17 @@
+using System;
+using NexonKernel.Kernel;
+using System.Drawing;
+using Cosmos.System.Graphic;
+using NexonKernel.Application;
 
+namespace NexonKernel.Application {
+    public class Clock : App {
+        public override string Title = "Terminal";
+        public override void OnDraw(Canvas canvas) {
+            canvas.DrawString("Shell@NexonKernel: ~$ ", PCScreenFont.LoadFont("Sysfont/ComicSans.psf"), Color.Black, 110, 140);
+        }
+        public override void OnClick(int x, int y) {
+            // For example, focus or click button
+        }
+    }
+}

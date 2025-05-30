b92e7b6c4ece8a8ae64c1050456dd334c815b96e
P€pperMCGamer$ <113534696+pmgdev64@users.noreply.github.com>
2025-05-30 12:36:40 +0700

    Add files via upload

--- src/GlobalSys/Application/BlueArchive/BlueArchive.cs
+++ src/GlobalSys/Application/BlueArchive/BlueArchive.cs
@@ -1,1 +1,15 @@
+using System;
+using System.Collections.Generic;
+using Cosmos.System;
+using Unity.Engine;
+using Unity.Engine.CosmosGL;
+using NexonKernel.Application;
 
+namespace NexonKernel.Application {
+    public class BlueArchive : App {
+        public override string Title = "Blue Archive (CosmosGL [Built-in OpenGL Library for Cosmos OS])";
+        public override void OnDraw() {}
+        public override void OnClick() {}
+        public override void Update() {}
+    }
+}

d90f2736d7a90253d59e5a3d6c51a70f07906971
P€pperMCGamer$ <113534696+pmgdev64@users.noreply.github.com>
2025-05-30 12:37:39 +0700

    Add files via upload

--- src/GlobalSys/Application/Terminal/Terminal.cs
+++ src/GlobalSys/Application/Terminal/Terminal.cs
@@ -1,1 +1,17 @@
+using System;
+using NexonKernel;
+using System.Drawing;
+using Cosmos.System.Graphic;
+using NexonKernel.Application;
 
+namespace NexonKernel.Application {
+    public class Terminal : App {
+        public override string Title = "Terminal";
+        public override void OnDraw(Canvas canvas) {
+            canvas.DrawString("Shell@NexonKernel: ~$ ", PCScreenFont.LoadFont("Sysfont/ComicSans.psf"), Color.Black, 110, 140);
+        }
+        public override void OnClick(int x, int y) {
+            // For example, focus or click button
+        }
+    }
+}
